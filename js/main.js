587a463e2efdc2316a758304a9f787e693087f42
George Sumpster <gsumpster@gmail.com>
2017-07-24 20:13:42 -0700

    feat($all): added form.js, styles for form states

--- js/main.js
+++ js/main.js
@@ -82,3 +82,54 @@ TABLET.addListener(resetDom) // Reset DOM on activation of Tablet Media Query
 DESKTOP.addListener(resetDom) // Reset DOM on activation of Desktop Media Query
 document.getElementById('stats__prev').onclick = prevStat;
 document.getElementById('stats__next').onclick = nextStat;
+
+// Form.js
+
+function validateInput(input) {
+  if (input.value === '') {
+    return false;
+  }
+
+  return true;
+}
+
+function submit(e) {
+  e.preventDefault();
+  let data = {}
+
+  for (const input of e.target) {
+    if (input.tagName !== 'BUTTON') {
+      if (validateInput(input)) {
+        data[input.name] = input.value;
+      } else {
+        input.classList.add('form__invalid');
+        input.placeholder = "Field may not be empty!"
+        return false;
+      }
+    }
+  }
+
+  const headers = new Headers({
+    "Content-Type": "application/json",
+  });
+
+  const options = {
+    method: 'PUT',
+    body: JSON.stringify(data)
+  };
+
+  fetch('https://kzl0zkdru3.execute-api.us-west-2.amazonaws.com/testing/applications', options).then((response) =>{
+    if(response.ok) {
+      const submitButton = document.getElementById('form__submit');
+      submitButton.classList.add('form__success');
+      submitButton.innerHTML = "Application Sent!"
+      submitButton.disabled = true;
+
+      for (const input of e.target) {
+        input.disabled = true;
+      }
+    }
+  });
+}
+
+document.getElementsByTagName('form')[0].onsubmit = submit;
